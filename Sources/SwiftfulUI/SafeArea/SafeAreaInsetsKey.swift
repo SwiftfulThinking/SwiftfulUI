19d4b59cc88a557db5d271f7c18acd7acd47f35a
Swiftful Thinking <ndsarno16@gmail.com>
2022-06-10 23:38:51 -0400

    [Feature] Add SwipeUpViewBuilder

---
+++ Sources/SwiftfulUI/SafeArea/SafeAreaInsetsKey.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nick Sarno on 6/10/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+//     @Environment(\.safeAreaInsets) private var safeAreaInsets
+
+extension UIApplication {
+    var connectedScenesKeyWindow: UIWindow? {
+        connectedScenes
+            .compactMap {
+                $0 as? UIWindowScene
+            }
+            .flatMap {
+                $0.windows
+            }
+            .first {
+                $0.isKeyWindow
+            }
+    }
+}
+
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        UIApplication.shared.connectedScenesKeyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+private extension UIEdgeInsets {
+    var swiftUiInsets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
+
