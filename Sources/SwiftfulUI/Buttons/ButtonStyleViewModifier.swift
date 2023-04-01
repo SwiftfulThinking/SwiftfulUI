0df9aeab45198d78be5b8f56e16e62be02e15637
Nick <hello@swiftful-thinking.com>
2023-04-01 12:17:37 -0400

    [Feature] Add asButton(type completion

--- Sources/SwiftfulUI/Buttons/ButtonStyleViewModifier.swift
+++ Sources/SwiftfulUI/Buttons/ButtonStyleViewModifier.swift
@@ -29,14 +29,31 @@ struct ButtonStyleViewModifier: ButtonStyle {
 
 }
 
+enum ButtonType {
+    case press, opacity, tap
+}
+
 public extension View {
     
     /// Wrap a View in a Button and add a custom ButtonStyle.
-    func asButton(scale: CGFloat = 0.95, opacity: Double = 1, brightness: Double = 0, action: @escaping () -> Void) -> some View {
+    func asButton(scale: CGFloat = 0.95, opacity: Double = 1, brightness: Double = 0, action: @escaping @MainActor () -> Void) -> some View {
         Button(action: action) {
             self
         }
         .buttonStyle(ButtonStyleViewModifier(scale: scale, opacity: opacity, brightness: brightness))
+    }
+    
+    func asButton(_ type: ButtonType = .tap, action: @escaping @MainActor () -> Void) -> some View {
+        switch type {
+        case .press:
+            self.asButton(scale: 0.975, action: action)
+        case .opacity:
+            self.asButton(scale: 1, opacity: 0.85, action: action)
+        case .tap:
+            self.onTapGesture {
+                action()
+            }
+        }
     }
     
 }
