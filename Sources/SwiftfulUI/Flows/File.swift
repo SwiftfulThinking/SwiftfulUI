67f8cb7ad3034bf22036a46e32ea74d79c4fd7ca
Nick <hello@swiftful-thinking.com>
2023-11-12 12:25:04 -0500

    [Feature] start cleaning up

--- Sources/SwiftfulUI/Flows/File.swift
+++ Sources/SwiftfulUI/Flows/File.swift
@@ -20,44 +20,28 @@ import SwiftUI
 //}
 //static func buildBlock<each Content>(_ content: repeat each Content) -> TupleView<(repeat each Content)> where repeat each Content : View
 
-struct AppView: View {
+/// A view displays one of two views based on a Boolean
+struct AnyConditionalView: View {
     
     let showView1: Bool
     let view1: () -> any View
     let view2: () -> any View
     
     var body: some View {
-//        ZStack {
-//            ZStack {
-                if showView1 {
-                    AnyView(view1())
-                }
-//            }
-//            .zIndex(showView1 ? 2 : 1)
-            
-//            ZStack {
-                if !showView1 {
-                    AnyView(view2())
-                }
-//            }
-//            .zIndex(showView1 ? 1 : 2)
-//        }
+        if showView1 {
+            AnyView(view1())
+        }
+        if !showView1 {
+            AnyView(view2())
+        }
     }
 }
 
-
-// var data: [(String, [(String, [String])])]
-
+/// A data model that contains itself, and a child array of it's own Type
 struct AnyRecursiveModel<Value:Identifiable>: Hashable, Identifiable {
     var id: AnyHashable
     var value: Value
     var children: [AnyRecursiveModel<Value>]?
-
-//    init<Model: HasRecursiveChildren>(_ model: Model) where Model.Value == Value {
-//        self.id = AnyHashable(model.id)
-//        self.value = model.value
-//        self.children = model.children?.map(AnyRecursiveModel.init)
-//    }
     
     init(value: Value) {
         self.id = value.id
@@ -78,81 +62,76 @@ struct AnyRecursiveModel<Value:Identifiable>: Hashable, Identifiable {
     }
 }
 
-//func asAnyRecursiveModel<T>(projectId: String) -> [AnyRecursiveModel<T>] {
-//    convertToRecursiveObject
-//}
-
-func convertToRecursiveModel<T>(_ items: [T]) -> [AnyRecursiveModel<T>] where T : Identifiable {
-    guard let first = items.first else { return [] }
+extension Array where Element : Identifiable {
     
-    var result: [AnyRecursiveModel<T>] = []
-    var remainingItems: [T] = items
-    
-    var object = AnyRecursiveModel(value: first)
-    
-    remainingItems.remove(at: 0)
-    let children = convertToRecursiveModel(remainingItems)
-    if !children.isEmpty {
-        object.children = children
+    /// Convert any array into AnyRecursiveModel, where Value is the first element in the Array
+    ///
+    /// For exampel:
+    ///
+    /// - let array = [1,2,3, 4, 5, 6]
+    /// - let newValue = array.asAnyRecursiveModel()
+    /// - then newValue will be:
+    /// - let newValue [(1, [(2, [(3, [(4, [(5, [])])])])])]
+    ///
+    func asAnyRecursiveModel() throws -> AnyRecursiveModel<Element> {
+        let items = self
+        
+        guard let first = items.first else { 
+            throw URLError(.dataNotAllowed)
+        }
+        
+        var remainingItems: [Element] = items
+        
+        var object = AnyRecursiveModel(value: first)
+        
+        remainingItems.remove(at: 0)
+        let children = remainingItems.asArrayOfAnyRecursiveModel()
+        if !children.isEmpty {
+            object.children = children
+        }
+        
+        return object
     }
     
-    result.append(object)
-    return result
+    private func asArrayOfAnyRecursiveModel() -> [AnyRecursiveModel<Element>] {
+        let items = self
+        
+        guard let first = items.first else { return [] }
+        
+        var result: [AnyRecursiveModel<Element>] = []
+        var remainingItems: [Element] = items
+        
+        var object = AnyRecursiveModel(value: first)
+        
+        remainingItems.remove(at: 0)
+        let children = remainingItems.asArrayOfAnyRecursiveModel()
+        if !children.isEmpty {
+            object.children = children
+        }
+        
+        result.append(object)
+        return result
+    }
+
 }
 
-//func convertToRecursiveModel<T>(_ items: [T]) -> [AnyRecursiveModel<T>] where T : Identifiable {
-//    
-//    var result: [AnyRecursiveModel<T>] = []
-//    var remainingItems: [T] = items
-//    
-//    for item in items {
-//        var object = AnyRecursiveModel(value: item)
-//        
-//        remainingItems.remove(at: 0)
-//        let children = convertToRecursiveModel(remainingItems)
-//        if !children.isEmpty {
-//            object.children = children
-//        }
-//        
-//        result.append(object)
-//    }
-//    
-//    return result
-//}
-
-
-
-
-//struct AnyRecursiveRow<T:Identifiable, Row:View, Header:View>: View {
-//    
-//    let items: [AnyRecursiveModel<T>]
-//    @ViewBuilder let row: (AnyRecursiveModel<T>) -> Row
-//    @ViewBuilder let header: () -> Header
-//    
-//    var body: some View {
-//        DisclosureGroup {
-//            ForEach(items) { item in
-//                row(item)
-//                    .listRowSeparator(.hidden)
-//
-//
 
 struct AnyRecursiveView<T:Identifiable>: View {
     
     let selection: T?
     let items: [T]
-    let recursiveItems: [AnyRecursiveModel<T>]
+    let recursiveItem: AnyRecursiveModel<T>?
     let view: (T?) -> any View
     
     init(selection: T?, items: [T], view: @escaping (T?) -> any View) {
         self.selection = selection
         self.items = items
-        self.recursiveItems = convertToRecursiveModel(items)
+        self.recursiveItem = try? items.asAnyRecursiveModel()
         self.view = view
     }
 
     var body: some View {
-        AppView(
+        AnyConditionalView(
             showView1: selection?.id == items.first?.id,
             view1: {
                 AnyView(view(items.first))
@@ -160,7 +139,7 @@ struct AnyRecursiveView<T:Identifiable>: View {
             view2: {
                 RecursiveView(
                     selection: selection,
-                    items: recursiveItems.first?.children ?? [],
+                    items: recursiveItem?.children ?? [],
                     view: { value in
                         AnyView(view(value?.value))
                     }
@@ -177,7 +156,7 @@ struct RecursiveView<T:Identifiable>: View {
     let view: (AnyRecursiveModel<T>?) -> any View
     
     var body: some View {
-        AppView(
+        AnyConditionalView(
             showView1: selection?.id == items.first?.id,
             view1: {
                 AnyView(view(items.first))
@@ -199,61 +178,6 @@ struct RecursiveView<T:Identifiable>: View {
     }
 }
 
-//struct PreviewView<T:Identifiable>: View {
-//    
-//    let selection: AnyRecursiveModel<T>?
-//    let items: [AnyRecursiveModel<T>]
-//    let view: (AnyRecursiveModel<T>?) -> any View
-//    
-//    var body: some View {
-//        ZStack {
-//            ForEach(items, id: \.self) { item in
-//                AppView(
-//                    showView1: selection?.value.id == item.value.id,
-//                    view1: {
-//                        AnyView(view(item))
-//                    },
-//                    view2: {
-//                        if let children = item.children {
-//                            PreviewView(
-//                                selection: selection,
-//                                items: item.children ?? [],
-//                                view: { value in
-//    //                                AnyView(view(item))
-//                                    Color.orange
-//                                }
-//                            )
-//                        } else {
-//                            Text("err: \(item.children?.count ?? 998)")
-//                        }
-////                        if let children = item.children {
-////                        ZStack {
-////                            Color.blue
-//                            
-//                            
-////                            PreviewView(
-////                                selection: selection,
-////                                items: item.children ?? [],
-////                                view: { _ in
-////                                    AnyView(view(item))
-////                                }
-////                            )
-////                        }
-////                        } else {
-////                            Text("Dead:: \(item.children?.count ?? 987)")
-////                        }
-//                    }
-//                )
-//            }
-//        }
-//    }
-//}
-
-
-
-
-
-
 struct Item: Identifiable {
     var id: String {
         text
@@ -270,7 +194,7 @@ struct AnotherView: View {
         Item(text: "one"),
         Item(text: "two"),
         Item(text: "three"),
-//        Item(text: "four"),
+        Item(text: "four"),
     ]
 //    @State var questions = convertToRecursiveModel([
 //        Item(text: "one"),
@@ -279,27 +203,18 @@ struct AnotherView: View {
 ////        Item(text: "four"),
 //    ])
     
+    @State private var showOnboarding: Bool = false
+    
     var body: some View {
         ZStack {
-//            ForEach(questions, id: \.self) { value in
-//                HStack {
-//                    Text(value.value.text)
-//                    Text("\(value.children?.count ?? 999)")
-//                }
+//            ForEach(0..<10) { x in
+//            }
+//            AnyRecursiveView(selection: x, items: 0..<10) { x in
 //                
-//                ForEach(value.children!, id: \.self) { vx in
-//                    HStack {
-//                        Text(vx.value.text)
-//                        Text("\(vx.children?.count ?? 999)")
-//                    }
-//                }
 //            }
             
+            
             AnyRecursiveView(selection: selection, items: questions) { value in
-                
-//            }
-//            
-//            PreviewView(selection: questions[showIndex], items: questions) { value in
                 Rectangle()
                     .fill(
                         value?.text == "one" ? Color.red :
@@ -312,29 +227,10 @@ struct AnotherView: View {
                         Text(value?.text ?? "nah")
                     )
                     .transition(AnyTransition.move(edge: value?.id == questions.first?.id ? .leading : .trailing))
-//                    .id(value?.id)
             }
         }
-        .animation(.smooth, value: selection?.id)
-        
-//        PreviewView(items: questions, view: { value in
-//            
-//        }
-//        )
-//        PreviewView(showIndex: showIndex, data: [
-//            ("one", ["two", ["three", ["Four"]]])
-//        ], view1: { data in
-//            Text(data ?? "nah")
-//        }
-//        )
-//        PreviewView(showIndex: showIndex, view1: {
-//            Rectangle()
-//                .fill(Color.red)
-//                .transition(AnyTransition.move(edge: .leading))
-//        })
+        .animation(.linear, value: selection?.id)
         .onTapGesture {
-            //            showSignIn.toggle()
-            
             if let selection, let index = questions.firstIndex(where: { $0.id == selection.id }),
                questions.indices.contains(index + 1) {
                 self.selection = questions[index + 1]
